f30b06aefb68c4a52cc40ced1335490b3c5d8784
Mohammad Danish <102878669+mddanish10@users.noreply.github.com>
2025-08-25 01:06:55 +0530

    Time: 4 ms (43.55%), Space: 76.8 MB (76.65%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,28 +1,45 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        //Brute force
-
-        int n=nums.length;
-        List<Integer> arrpos = new ArrayList<>(n/2);
-        List<Integer> arrneg = new ArrayList<>(n/2);
        
 
-        for(int i=0;i<n;i++){
-            if (nums[i]<0)
-            {
-                arrneg.add(nums[i]);
-            }
-            else {
-                arrpos.add(nums[i]);
-        }}
+        int n=nums.length;
 
-        for(int i=0;i<n/2;i++){
-            nums[2*i]=arrpos.get(i);
-            nums[2*i+1]=arrneg.get(i);
-        }
+         //Brute force
+        // List<Integer> arrpos = new ArrayList<>(n/2);
+        // List<Integer> arrneg = new ArrayList<>(n/2);
+       
 
-        return nums;
+        // for(int i=0;i<n;i++){
+        //     if (nums[i]<0)
+        //     {
+        //         arrneg.add(nums[i]);
+        //     }
+        //     else {
+        //         arrpos.add(nums[i]);
+        // }}
 
+        // for(int i=0;i<n/2;i++){
+        //     nums[2*i]=arrpos.get(i);
+        //     nums[2*i+1]=arrneg.get(i);
+        // }
+       
+       //Optimal Solution
+    int pos=0;
+    int neg=1;
+    int arr [] = new int[n];
+    for(int i=0;i<n;i++){
+
+         if (nums[i]>0 && pos<n){
+           arr[pos]=nums[i];
+            pos+=2;
+         } 
+         if(nums[i]<0 && neg<n) {
+          arr[neg]=nums[i];
+            neg+=2;
+         }
+
+    }
+        return arr;
         
     }
   } 
