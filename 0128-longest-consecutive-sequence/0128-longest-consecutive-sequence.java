6dfd691486df69cc440166670d3b9cc22848955e
Mohammad Danish <102878669+mddanish10@users.noreply.github.com>
2025-08-26 22:45:58 +0530

    Time: 30 ms (70.07%), Space: 68 MB (22.3%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,28 +1,58 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        int[] arr = nums;
-        int count = 1;
-        int ccount = 1;
+        //better Solution
+        // int n = nums.length;
+        // Arrays.sort(nums);
+        // int[] arr = nums;
+        // int count = 1;
+        // int ccount = 1;
 
-        if (arr.length==0){
+        // if (arr.length==0){
+        //     return 0;
+        // }
+
+        // for (int i = 1; i < n; i++) {
+        //     if (arr[i] - arr[i - 1] == 1) {
+        //         ccount++;
+
+        //     } else if (arr[i] == arr[i - 1] ) {
+
+        //     } else {
+        //         ccount = 1;
+        //     }
+        //      count = Math.max(count, ccount);
+
+        // }
+        // return count;
+
+        //Optimal Solution
+
+        if (nums.length == 0) {
             return 0;
         }
-       
-        for (int i = 1; i < n; i++) {
-            if (arr[i] - arr[i - 1] == 1) {
-                ccount++;
-               
-            } else if (arr[i] == arr[i - 1] ) {
-               
 
-            } else {
-                ccount = 1;
-            }
-             count = Math.max(count, ccount);
-
+        int count = 1;
+        Set<Integer> hs = new HashSet<>();
+        for (int arr : nums) {
+            hs.add(arr);
         }
+
+        for (int arr : hs) {
+            if (!hs.contains(arr - 1)) {
+               int cnum = arr;
+                int ccount = 1;
+
+            while (hs.contains(cnum + 1)) {
+                cnum++;
+                ccount++;
+
+            }
+            count = Math.max(count, ccount);
+            }
+                
+            
+        }
+
         return count;
 
     }
