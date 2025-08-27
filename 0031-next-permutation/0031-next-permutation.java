d324160b7199e0b53e7b9d599aafcf89b6878768
Mohammad Danish <102878669+mddanish10@users.noreply.github.com>
2025-08-27 13:46:12 +0530

    Time: 0 ms (100%), Space: 42.9 MB (95.52%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -2,45 +2,50 @@ class Solution {
     public void nextPermutation(int[] nums) {
 
         int n = nums.length;
-        int index = -1;
-
+        int bp = -1;
+        //find breakpoint and come out of the loop;
         for (int i = n - 2; i >= 0; i--) {
             if (nums[i] < nums[i + 1]) {
-                index = i;
+                bp = i;
+                break;
+
+            }
+        }
+
+        if (bp == -1) {
+            rev(nums, 0, n - 1);
+            return;
+        }
+
+        for (int i = n - 1; i >= bp + 1; i--) {
+            if (nums[i] > nums[bp]) {
+                swap(nums, bp, i);
                 break;
             }
         }
 
-        if (index == -1) {
-            reverse(nums, 0, n - 1);
-            return;
-        }
-
-        for (int i = n - 1; i > index; i--) {
-            if (nums[i] > nums[index]){ swap(nums, index, i);
-            break;}
-               
-        }
-
-        reverse(nums, index + 1, n - 1);
+        rev(nums, bp + 1, n - 1);
 
     }
 
-    public static void swap(int[] nums, int a, int b) {
+    public static void swap(int nums[], int a, int b) {
+
         int temp = nums[a];
         nums[a] = nums[b];
         nums[b] = temp;
 
     }
 
-    public static void reverse(int[] nums, int first, int last) {
-        while (first < last) {
-            int temp = nums[first];
-            nums[first] = nums[last];
-            nums[last] = temp;
-            first++;
-            last--;
+    public static void rev(int nums[], int a, int b) {
+
+        while (a < b) {
+            int temp = nums[a];
+            nums[a] = nums[b];
+            nums[b] = temp;
+            a++;
+            b--;
         }
+
     }
 
 }
