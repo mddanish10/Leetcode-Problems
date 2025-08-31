457193265e68cd957f80c4470170aba5e54972a3
Mohammad Danish <102878669+mddanish10@users.noreply.github.com>
2025-08-31 20:17:28 +0530

    Time: 0 ms (100%), Space: 42.3 MB (59.15%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -2,23 +2,50 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
 
-        int[][] mat = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        //brute force
+        // int[][] mat = new int[n][n];
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
 
-                mat[j][n - 1 - i] = matrix[i][j];
+        //         mat[j][n - 1 - i] = matrix[i][j];
 
+        //     }
+
+        // }
+
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+
+        //         matrix[i][j] = mat[i][j];
+
+        //     }
+
+        // }
+
+        //optimal
+        //transpose;
+        for (int i = 0; i < n - 1; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
-
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        //reverse
 
-                matrix[i][j] = mat[i][j];
-
+        for(int i =0;i<n;i++){
+            int start=0;
+            int end=n-1;
+            while(start <end){
+                int temp=matrix[i][start];
+                matrix[i][start]=matrix[i][end];
+               matrix[i][end]=temp;
+                start++;
+                end--;
             }
-
         }
 
     }
