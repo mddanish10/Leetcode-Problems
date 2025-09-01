3a1e9775d292b1e09b63b370e9da9f783303410d
Mohammad Danish <102878669+mddanish10@users.noreply.github.com>
2025-09-01 23:38:53 +0530

    Time: 1 ms (90.12%), Space: 45.6 MB (7.41%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -3,16 +3,35 @@ class Solution {
 
         int n = nums.length;
         int largest = Integer.MIN_VALUE;
-
-        for (int i = 0; i < n; i++) {
-            int product = 1;
-            for (int j = i; j < n; j++) {
-                product = product * nums[j];
-                largest = Math.max(largest, product);
-            }
+        
+        // brute force 
+        // for (int i = 0; i < n; i++) {
+        //     int product = 1;
+        //     for (int j = i; j < n; j++) {
+        //         product = product * nums[j];
+        //         largest = Math.max(largest, product);
+        //     }
            
 
+        // }
+
+        //optimal solution
+        //1-if all positive than its 
+        //2-if even negative
+        //3-if odd negative
+        //4 if any zero than value be after the zero
+         int prefix =1;
+            int suffix =1;
+        for(int i=0;i<n;i++){
+           
+           if ( prefix == 0 ) {prefix=1;}
+           if ( suffix == 0) {suffix =1;}
+           prefix = prefix * nums[i];
+           suffix = suffix * nums[n-1-i];
+           largest= Math.max(largest, Math.max(prefix,suffix));
         }
+
+
         return largest;
 
     }
